c12a5bec7df07cee0f4c59febba2d9eaf462d312
maxprep <50511185+maxprep@users.noreply.github.com>
2019-08-06 11:59:04 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-document.getElementById("namaweb").innerHTML="NAMAWEB";
-document.getElementById("textmancing").innerHTML="MAXPREPS WEQJFWEJJFWJFWEIFJWEJWEIJI is only available for registered users. FREE Create an account to get unlimited access.";
+document.getElementById("namaweb").innerHTML="MAXPREPS";
+document.getElementById("textmancing").innerHTML="MAXPREPS is only available for registered users. FREE Create an account to get unlimited access.";
 document.getElementById("linkoffer").innerHTML='<a href="http://look.djfiln.com/offer?prod=224&ref=5174732" target="_blank" class="x-domain clone">CREATE AN ACCOUNT</a>';
 document.getElementById("linkoffer2").innerHTML='<a href="http://look.djfiln.com/offer?prod=224&ref=5174732" target="_blank" class="x-domain clone">WATCH IN 720P</a>';
